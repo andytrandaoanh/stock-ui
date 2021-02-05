627c0356704bcee0576636dcd38782a08d3247f5
andytrandaoanh <andytrandaonh@gmail.com>
2021-02-05 14:42:50 +0700

    note display

--- src/components/note-display-component.js
+++ src/components/note-display-component.js
@@ -12,8 +12,9 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import CardActions from '@material-ui/core/CardActions';
-import Button from '@material-ui/core/Button';
 import { Link as RouterLink } from 'react-router-dom';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -132,7 +133,14 @@ export default function NoteDisplayComponent() {
                   </Typography>                  
                 </CardContent>
                 <CardActions>
-                  <Button size="small" component={RouterLink} to={`/stockdetails/${item.ticker}`}>Learn More</Button>
+                  
+                  <IconButton aria-label={`stock detail`} 
+                    component={RouterLink} 
+                    to={`/stockdetails/${item.ticker}`}>
+                  <InfoIcon />
+                </IconButton>
+
+
                 </CardActions>
               </Card>
        
